c04908b8f1515ea2333eeb4638db8a41a29b20d6
jmo-02 <juan.moreno.murillo@correounivalle.edu.co>
2025-10-08 22:11:38 -0500

    commit para configurar firebase y package.json para despliegue en render

--- backend/firebase/config.js
+++ backend/firebase/config.js
@@ -8,8 +8,11 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Leer el archivo JSON con las credenciales del servicio
 const serviceAccount = JSON.parse(
-  readFileSync(`${__dirname}/serviceAccountKey.json`, "utf8")
+  readFileSync("/etc/secrets/serviceAccountKey.json", "utf8")
 );
+// const serviceAccount = JSON.parse(
+//   readFileSync(`${__dirname}/serviceAccountKey.json`, "utf8")
+// );
 
 // Inicializar la app de Firebase Admin
 admin.initializeApp({
