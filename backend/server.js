e42311fc21f9622c45a912f857b24529544bb699
jmo-02 <juan.moreno.murillo@correounivalle.edu.co>
2025-10-09 21:17:30 -0500

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 // backend/server.js
 import express from "express";
 import cors from "cors";
-import ventasRoutes from "./routes/ventasRoutes.js";
+import fiestasRoutes from "./routes/fiestasRoutes.js";
 
 const app = express();
 const PORT = 5000;
@@ -9,8 +9,8 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
-// Rutas de ventas
-app.use("/api/ventas", ventasRoutes);
+// Rutas de fiestas
+app.use("/api/fiestas", fiestasRoutes);
 
 app.listen(PORT, () => {
   console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
